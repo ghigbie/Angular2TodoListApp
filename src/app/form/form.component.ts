0cedd392fc8a2527897de9b808d8ae8ebd9c635c
George Higbie <gehigbie@gmail.com>
2017-10-27 17:43:53 +0000

    Import angualr forms module

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
-  todos = ['Go skydiving', 'Play video games', 'Eat oranges', 'Walk the doggie'];
+  
+  newTodo:string = '';
+  todos:string[] = ['Go skydiving', 'Play video games', 'Eat oranges', 'Walk the doggie'];
+  
+  onSubmit(){
+    this.todos.push(this.newTodo);
+    this.newTodo = '';
+  }
+  
   constructor() { }
 
   ngOnInit() {
